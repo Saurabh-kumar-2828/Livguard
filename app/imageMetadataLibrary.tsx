270f2aa5aa1823a2bacd00b375e199d7e07a4b30
growthjockey-aresh <aresh@growthjockey.com>
2023-02-17 11:35:07 +0530

    Changes

--- app/imageMetadataLibrary.tsx
+++ app/imageMetadataLibrary.tsx
@@ -1,18 +1,116 @@
 import {ImageMetadata} from "~/global-common-typescript/typeDefinitions";
 
 export const imageMetadataLibrary: {[relativePath: string]: ImageMetadata | undefined} = {
+    "/livguard/bottom-bar/contact-us.png": {
+        "width": 32,
+        "height": 32,
+        "altText": "Dummy alt text",
+        "blurHash": "LBK1%f-;IU?b~q%Mxu%MM{M{M{Rj",
+        "placeholderColor": "#080808"
+    },
+    "/livguard/bottom-bar/dealer-locator.png": {
+        "width": 32,
+        "height": 32,
+        "altText": "Dummy alt text",
+        "blurHash": "L8KK==~qfQ~q~qofoft7ayj[j[ay",
+        "placeholderColor": "#b8b8b8"
+    },
+    "/livguard/bottom-bar/home-inverter.png": {
+        "width": 32,
+        "height": 32,
+        "altText": "Dummy alt text",
+        "blurHash": "L8KK==~qfQ~q~qofj[ofofj[fQay",
+        "placeholderColor": "#b8b8b8"
+    },
+    "/livguard/bottom-bar/other-products.png": {
+        "width": 32,
+        "height": 32,
+        "altText": "Dummy alt text",
+        "blurHash": "L8KK==~qay~q~qofj[ofWBj[ayay",
+        "placeholderColor": "#b8b8b8"
+    },
+    "/livguard/favicon-128.png": {
+        "width": 128,
+        "height": 128,
+        "altText": "Dummy alt text",
+        "blurHash": "L~RJ{Q?^YO$j-pXmrsRPWBniWpkC",
+        "placeholderColor": "#e81828"
+    },
+    "/livguard/favicon-256.png": {
+        "width": 256,
+        "height": 256,
+        "altText": "Dummy alt text",
+        "blurHash": "L~RJ?A?^YP$j-pXmr?RPWBniWVf+",
+        "placeholderColor": "#e81828"
+    },
+    "/livguard/favicon-32.png": {
+        "width": 32,
+        "height": 32,
+        "altText": "Dummy alt text",
+        "blurHash": "L~RTg0?^lS$P%MXmniVtWBniW;of",
+        "placeholderColor": "#e81828"
+    },
+    "/livguard/favicon-512.png": {
+        "width": 512,
+        "height": 512,
+        "altText": "Dummy alt text",
+        "blurHash": "L~RJ?A?^cs$j-pTJr?VsWBniW;kC",
+        "placeholderColor": "#080808"
+    },
+    "/livguard/favicon-64.png": {
+        "width": 64,
+        "height": 64,
+        "altText": "Dummy alt text",
+        "blurHash": "L~RJ]I?^YP$j-pXmr?RPWBniWpkC",
+        "placeholderColor": "#e81828"
+    },
+    "/livguard/header/akshay.png": {
+        "width": 318,
+        "height": 438,
+        "altText": "Dummy alt text",
+        "blurHash": "LaHvYj}FE2W:I;NaI:NHKNK3Nvso",
+        "placeholderColor": "#080808"
+    },
+    "/livguard/home/hero.jpg": {
+        "width": 390,
+        "height": 515,
+        "altText": "Dummy alt text",
+        "blurHash": "LBAmk[?F-nx].TxZtR%fo#R-n%t7",
+        "placeholderColor": "#787878"
+    },
     "/livguard/logo-128.png": {
         "width": 128,
         "height": 31,
-        "altText": "Livguard Logo",
-        "blurHash": "L4BTKg]SsU,r|v|v|v,rwxo1E~Nv",
-        "placeholderColor": "#ff0000"
+        "altText": "Dummy alt text",
+        "blurHash": "L~R{c9xGae%g~qn%j]t7WVfkj[fk",
+        "placeholderColor": "#f8f8f8"
     },
     "/livguard/logo-256.png": {
         "width": 256,
-        "height": 62,
-        "altText": "Livguard Logo",
-        "blurHash": "L4BTKg]SsU,r|v|v|v,rwxo1E~Nv",
-        "placeholderColor": "#ff0000"
+        "height": 63,
+        "altText": "Dummy alt text",
+        "blurHash": "L|R{c9xGae%g~qjZkCt7WBbHkCfk",
+        "placeholderColor": "#080808"
     },
+    "/livguard/logo-32.png": {
+        "width": 32,
+        "height": 8,
+        "altText": "Dummy alt text",
+        "blurHash": "L{R.=R$*WByD~qs:j[ozRjofa|a|",
+        "placeholderColor": "#f8f8f8"
+    },
+    "/livguard/logo-512.png": {
+        "width": 512,
+        "height": 126,
+        "altText": "Dummy alt text",
+        "blurHash": "L~R.[ZxGae%g~qn%kCt7WBbHkCj[",
+        "placeholderColor": "#080808"
+    },
+    "/livguard/logo-64.png": {
+        "width": 64,
+        "height": 16,
+        "altText": "Dummy alt text",
+        "blurHash": "L]R.[Z$jjZ%g~qoLoftRRjfkofj[",
+        "placeholderColor": "#f8f8f8"
+    }
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -118,36 +118,57 @@ function HeroSection({userPreferences}: {userPreferences: UserPreferences}) {
 
 function StickyBottomBar({userPreferences}: {userPreferences: UserPreferences}) {
     return (
-        <div className="tw-sticky tw-bottom-0 lg-bg-secondary-300 tw-rounded-t-lg tw-grid tw-grid-cols-[2fr_auto_1fr_auto_1fr_auto_1fr_auto_1fr_auto_2fr] tw-py-[0.8125rem] tw-text-center">
+        <div className="tw-sticky tw-bottom-0 lg-bg-secondary-300 tw-rounded-t-lg tw-grid tw-grid-cols-[1fr_auto_1fr_auto_1fr_auto_1fr_auto_1fr_auto_1fr] tw-py-[0.8125rem] tw-text-center">
             <div className="tw-row-start-1 tw-col-start-2 tw-flex tw-flex-col tw-items-center tw-text-center">
-                <div className="tw-w-8 tw-h-8 tw-rounded-full lg-bg-secondary-500" />
+                {/* <div className="tw-w-8 tw-h-8 tw-rounded-full lg-bg-secondary-500" /> */}
+                <FixedWidthImage relativePath="/livguard/bottom-bar/home-inverter.png" width="2rem" imageCdnProvider={ImageCdnProvider.Imgix} />
                 <VerticalSpacer className="tw-h-1" />
-                <div className="lg-text-icon">Home<br />Inverters</div>
+                <div className="lg-text-icon">
+                    Home
+                    <br />
+                    Inverters
+                </div>
             </div>
 
             <div className="tw-row-start-1 tw-col-start-4 tw-flex tw-flex-col tw-items-center">
-                <div className="tw-w-8 tw-h-8 tw-rounded-full lg-bg-secondary-500" />
+                <FixedWidthImage relativePath="/livguard/bottom-bar/other-products.png" width="2rem" imageCdnProvider={ImageCdnProvider.Imgix} />
                 <VerticalSpacer className="tw-h-1" />
-                <div className="lg-text-icon">Other<br />Products</div>
+                <div className="lg-text-icon">
+                    Other
+                    <br />
+                    Products
+                </div>
             </div>
 
             {/* TODO: Figure out the top properly */}
             <div className="tw-row-start-1 tw-col-start-6 tw-h-[2.875rem] tw-relative tw-top-[-1.7rem] tw-flex tw-flex-col tw-items-center tw-overflow-visible">
-                <div className="tw-w-16 tw-h-16 tw-rounded-full lg-bg-primary-500 tw-flex-none" />
+                <img src="https://growthjockey.imgix.net/livguard/bottom-bar/load-calculator.gif" width={64} height={64} className="tw-rounded-full" />
                 <VerticalSpacer className="tw-h-1" />
-                <div className="lg-text-icon">Plan My<br />Power</div>
+                <div className="lg-text-icon">
+                    Plan My
+                    <br />
+                    Power
+                </div>
             </div>
 
             <div className="tw-row-start-1 tw-col-start-8 tw-flex tw-flex-col tw-items-center">
-                <div className="tw-w-8 tw-h-8 tw-rounded-full lg-bg-secondary-500" />
+                <FixedWidthImage relativePath="/livguard/bottom-bar/dealer-locator.png" width="2rem" imageCdnProvider={ImageCdnProvider.Imgix} />
                 <VerticalSpacer className="tw-h-1" />
-                <div className="lg-text-icon">Find My<br />Dealer</div>
+                <div className="lg-text-icon">
+                    Find My
+                    <br />
+                    Dealer
+                </div>
             </div>
 
             <div className="tw-row-start-1 tw-col-start-10 tw-flex tw-flex-col tw-items-center">
-                <div className="tw-w-8 tw-h-8 tw-rounded-full lg-bg-secondary-500" />
+                <FixedWidthImage relativePath="/livguard/bottom-bar/contact-us.png" width="2rem" imageCdnProvider={ImageCdnProvider.Imgix} />
                 <VerticalSpacer className="tw-h-1" />
-                <div className="lg-text-icon">Contact<br />Support</div>
+                <div className="lg-text-icon">
+                    Contact
+                    <br />
+                    Support
+                </div>
             </div>
         </div>
     );
@@ -494,7 +515,6 @@ export function TransformingLives({userPreferences}: {userPreferences: UserPrefe
             productName: `${getVernacularString("review4ProductName", userPreferences.language)}`,
         },
     ];
-
 
     return (
         <div className="lg-px-screen-edge">
