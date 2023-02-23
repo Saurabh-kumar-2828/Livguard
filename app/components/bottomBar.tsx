6a591d07a83e46ad9bf63164a1d55d932927f309
growthjockey-aresh <aresh@growthjockey.com>
2023-02-23 16:06:38 +0530

    footer changes

--- app/components/bottomBar.tsx
+++ app/components/bottomBar.tsx
@@ -25,13 +25,17 @@ export function StickyBottomBar({userPreferences}: {userPreferences: UserPrefere
             <div className="tw-row-start-1 tw-col-start-4 tw-flex tw-flex-col tw-items-center tw-gap-y-2">
                 <Link
                     to="/category/inverters"
-                    className="tw-flex tw-flex-col tw-items-center tw-text-center tw-gap-y-2"
+                    className="tw-flex tw-flex-col tw-items-center tw-text-center tw-gap-y-2 tw-relative"
                 >
                     <object
                         data="https://files.growthjockey.com/livguard/icons/bottom-bar/2.svg"
                         className="tw-w-6 tw-h-6 dark:tw-invert tw-pointer-events-none"
                     />
                     <div className="lg-text-icon">{getVernacularString("bottomBarT2", userPreferences.language)}</div>
+
+                    <div className="lg-bg-primary-500 tw-rounded-full tw-text-icon tw-text-secondary-900-dark tw-absolute -tw-top-5 tw-right-0 tw-left-0 tw-mx-auto tw-text-center tw-text-[0.6rem]">
+                        Trending
+                    </div>
                 </Link>
             </div>
 
