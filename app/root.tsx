e00eaa193fa0bf0e9691a88a3fbd1816f65041eb
growthjockey-aresh <aresh@growthjockey.com>
2023-02-24 21:13:27 +0530

    Changes

--- app/root.tsx
+++ app/root.tsx
@@ -97,22 +97,7 @@ export default function() {
                 <noscript><img height="1" width="1" style={{display: "none"}} src="https://www.facebook.com/tr?id=635911646858607&ev=PageView&noscript=1" /></noscript>
                 {/* End Meta Pixel Code */}
 
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            !function(f,b,e,v,n,t,s)
-                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                            n.queue=[];t=b.createElement(e);t.async=!0;
-                            t.src=v;s=b.getElementsByTagName(e)[0];
-                            s.parentNode.insertBefore(t,s)}(window, document,'script',
-                            'https://connect.facebook.net/en_US/fbevents.js');
-                            fbq('init', '635911646858607');
-                            fbq('track', 'PageView');
-                        `,
-                    }}
-                />
+                <meta name="facebook-domain-verification" content="vvv1ovrlljfrtp4dnwttb9j964i14k" />
 
                 {/* <script
                     dangerouslySetInnerHTML={{
@@ -122,14 +107,6 @@ export default function() {
                             } else {
                                 document.documentElement.classList.remove("tw-dark");
                             }
-                        `,
-                    }}
-                /> */}
-
-                {/* <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            !function(e,t,a){var c=e.head||e.getElementsByTagName("head")[0],n=e.createElement("script");n.async=!0,n.defer=!0, n.type="text/javascript",n.src=t+"/static/js/widget.js?config="+JSON.stringify(a),c.appendChild(n)}(document,"https://app.engati.com",{bot_key:"169eda7edf434ed0",welcome_msg:true,branding_key:"default",server:"https://app.engati.com",e:"p" });
                         `,
                     }}
                 /> */}
