4a98157c6bf7238457dd7cc0ddf8478bbc49caa1
growthjockey-aresh <aresh@growthjockey.com>
2023-03-07 09:31:59 +0530

    Changes

--- app/routes/campaigns/energy-storage-solution.tsx
+++ app/routes/campaigns/energy-storage-solution.tsx
@@ -334,11 +334,11 @@ export function LimitlessEnergy({userPreferences, className}: {userPreferences: 
                                 </DefaultImageAnimation>
 
                                 <DefaultTextAnimation className="tw-row-start-4 tw-col-start-2">
-                                    <div className="lg-text-title1 tw-whitespace-pre-line">{getVernacularString(item.titleTextContentPiece, userPreferences.language)}</div>
+                                    <div className="lg-text-title1 tw-whitespace-pre-line tw-text-secondary-900-dark">{getVernacularString(item.titleTextContentPiece, userPreferences.language)}</div>
                                 </DefaultTextAnimation>
 
                                 <DefaultTextAnimation className="tw-row-start-6 tw-col-start-2">
-                                    <div className="lg-text-body">{getVernacularString(item.bodyTextContentPiece, userPreferences.language)}</div>
+                                    <div className="lg-text-body !tw-text-secondary-900-dark">{getVernacularString(item.bodyTextContentPiece, userPreferences.language)}</div>
                                 </DefaultTextAnimation>
                             </div>
                         )}
