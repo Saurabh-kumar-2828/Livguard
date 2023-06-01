f710558c66393e86494d5125bcfc7ad5d6545d44
Ravi Chaudhary <ravi.chaudhary@growthjockey.com>
2023-06-01 15:01:08 +0530

    added form data

--- app/routes/google-lead-form-callback.tsx
+++ app/routes/google-lead-form-callback.tsx
@@ -1,5 +1,6 @@
 import type {ActionFunction} from "@remix-run/node";
 import {getRequiredEnvironmentVariableNew} from "~/global-common-typescript/server/utilities.server";
+import {getSingletonValue, safeExecute} from "~/global-common-typescript/utilities/utilities";
 
 type ResponseData = {
     message: string;
@@ -29,8 +30,9 @@ export const action: ActionFunction = async ({request, params}) => {
         return new Response(JSON.stringify(actionData), {status: 401});
     }
 
+    const userData: Array<{column_name: string; string_value: string; column_id: string}> = requestBody.user_column_data;
+
     const leadId = requestBody.lead_id;
-    const userData = requestBody.user_column_data;
     const formId = requestBody.form_id;
     const campaignId = requestBody.campaign_id;
     const isTest = requestBody.is_test;
@@ -38,15 +40,45 @@ export const action: ActionFunction = async ({request, params}) => {
     const adgroupId = requestBody.adgroup_id;
     const creativeId = requestBody.creative_id;
 
+    const fullName =
+        safeExecute(
+            getSingletonValue,
+            userData.filter((item) => item["column_id"] == "FULL_NAME"),
+        )?.string_value ?? "";
+    const phoneNumber =
+        safeExecute(
+            getSingletonValue,
+            userData.filter((item) => item["column_id"] == "PHONE_NUMBER"),
+        )?.string_value ?? "";
+    const email =
+        safeExecute(
+            getSingletonValue,
+            userData.filter((item) => item["column_id"] == "EMAIL"),
+        )?.string_value ?? "";
+    const postalCode =
+        safeExecute(
+            getSingletonValue,
+            userData.filter((item) => item["column_id"] == "POSTAL_CODE"),
+        )?.string_value ?? "";
+    const city =
+        safeExecute(
+            getSingletonValue,
+            userData.filter((item) => item["column_id"] == "CITY"),
+        )?.string_value ?? "";
+
     const googleSheetData = {
         leadId: leadId,
-        userData: userData,
         formId: formId,
         campaignId: campaignId,
         isTest: isTest,
         gclId: gclId,
         adgroupId: adgroupId,
         creativeId: creativeId,
+        fullName: fullName,
+        phoneNumber: phoneNumber,
+        email: email,
+        postalCode: postalCode,
+        city: city,
     };
 
     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
