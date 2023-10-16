b219c8b00b7463e52d9d3607fa7f644a53492287
taranpreet-singh-29 <taranpreet.singh@growthjockey.com>
2023-10-16 16:35:36 +0530

    Perf fixes

--- app/root.tsx
+++ app/root.tsx
@@ -125,7 +125,7 @@ export default function Root() {
 
     useExternalScript(
         "https://toolassets.haptikapi.com/platform/javascript-xdk/production/loader.js",
-        {removeOnUnmount: true, appendInHead: true, timeoutDuration: 0},
+        {removeOnUnmount: true, appendInHead: true, timeoutDuration: 10000},
         isScreenSizeBelow ? false : true,
         () => {
             document.addEventListener("haptik_sdk", () => setHaptikLanguage(userPreferences.language));
@@ -134,21 +134,21 @@ export default function Root() {
 
     useExternalScript(
         `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5HRQL29');`,
-        {removeOnUnmount: true, appendInBody: true, addScriptAsText: true, timeoutDuration: 0},
+        {removeOnUnmount: true, appendInBody: true, addScriptAsText: true, timeoutDuration: 10000},
         // cookiesAccepted,
         true,
     );
 
     useExternalScript(
         `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '635911646858607');fbq('track', 'PageView');`,
-        {removeOnUnmount: true, appendInBody: true, addScriptAsText: true, timeoutDuration: 0},
+        {removeOnUnmount: true, appendInBody: true, addScriptAsText: true, timeoutDuration: 10000},
         // cookiesAccepted,
         true,
     );
 
     useExternalScript(
         `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "ganufjw8cz");`,
-        {removeOnUnmount: true, appendInBody: true, addScriptAsText: true, timeoutDuration: 0},
+        {removeOnUnmount: true, appendInBody: true, addScriptAsText: true, timeoutDuration: 10000},
         // cookiesAccepted,
         true,
     );
